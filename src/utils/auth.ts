230f92d1e24d5d2c681d8c70208627c545f3eba9
Edward Ramirez <Edward.Ramirez@coxautoinc.com>
2022-08-05 09:37:01 -0500

    feat: add change password handler

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyEventHeaders } from 'aws-lambda';
 
 export const extractBearerToken = (headers: APIGatewayProxyEventHeaders): string | null => {
+  console.log(headers.Authorization);
+  console.log(headers.authorization);
   if (headers.Authorization?.includes('Bearer')) {
     return headers.Authorization.split('Bearer ')[1];
   }
